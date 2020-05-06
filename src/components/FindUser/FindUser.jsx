da9540782f49f2370903b97b4bf66f4cc82450a3
tiurinea <eturin@gmail.com>
2020-05-06 12:37:49 +0300

    53

--- src/components/FindUser/FindUser.jsx
+++ src/components/FindUser/FindUser.jsx
@@ -2,19 +2,24 @@ import React from "react";
 import css from "./FindUser.module.css"
 import ItemContainer from "./Item/ItemContainer";
 
-const FindUser = (props) => {
-    let mJSXItems = props.mUsers.map(x => <ItemContainer key={x.id} id={x.id} />);
-    return (
-        <div className={css.FindUser}>
-            <p className={ css.P }>Пользователи</p>
-            <div>
-                { mJSXItems}
+class FindUser extends React.Component {
+    constructor(props) { super(props); }; /*нельзя заменить на стрелочную функцию*/
+    render = () => {
+        if(this.props.mUsers.length==0) this.props.getMore(this.props.page);
+
+        let mJSXItems = this.props.mUsers.map(x => <ItemContainer key={x.id} id={x.id} />);
+        return (
+            <div className={css.FindUser}>
+                <p className={ css.P }>Пользователи</p>
+                <div>
+                    { mJSXItems}
+                </div>
+                <div className={css.DivB}>
+                    <button className={css.Button} title='Показать ещё' onClick={ () => {this.props.getMore(this.props.page);} }>Показать ещё</button>
+                </div>
             </div>
-            <div className={css.DivB}>
-                <button className={css.Button} title='Показать ещё' onClick={ () => {props.getMore(props.page);} }>Показать ещё</button>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default FindUser;
